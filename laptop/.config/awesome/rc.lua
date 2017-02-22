5a10a922ff5c5e61a4b2f9efd227db08210f7955
Alex King <king110.py@gmail.com>
2017-02-22 10:31:05 +0330

    adds unclutter to startup scripts

--- laptop/.config/awesome/rc.lua
+++ laptop/.config/awesome/rc.lua
@@ -459,3 +459,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 os.execute('setxkbmap -option caps:super')
+os.execute('unclutter &')
