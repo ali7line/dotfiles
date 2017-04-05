3a26fc04bd028617c6eb65c2122f44530e41fb72
ali7line <soltani.110@gmail.com>
2017-04-05 12:22:47 +0430

    temp add set_stow for ubuntu 16.04

---
+++ set_stow_ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+stow -d qutebrowser/ -t $HOME qutebrowser-0.9
+stow -d awesome/ -t $HOME awesome-3.7
+stow bash/
+stow config/
+stow fonts/
+stow gimp/
+stow git/
+stow mpd/ 
+stow redshift/
+stow scripts/
+stow tmux/
+stow vim/
+stow mpv/
