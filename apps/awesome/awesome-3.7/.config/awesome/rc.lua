739bc0bf5a5fded9412a782614148a2ac3f07531
ali7line <soltani.110@gmail.com>
2018-01-11 10:54:08 +0330

    fix typo in awesome-3.7

--- apps/awesome/awesome-3.7/.config/awesome/rc.lua
+++ apps/awesome/awesome-3.7/.config/awesome/rc.lua
@@ -204,7 +204,6 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(battery.widget)
     right_layout:add(kbdcfg.widget)                                                                                                           
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
