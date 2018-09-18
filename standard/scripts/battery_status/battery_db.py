f581d60fdba69d1db8f86d42d07c34912593daab
ali7line <soltani.110@gmail.com>
2018-09-18 12:20:31 +0430

    adds database design

--- standard/scripts/battery_status/battery_db.py
+++ standard/scripts/battery_status/battery_db.py
@@ -1,9 +1,32 @@
 import sqlite3
 
 
-def create_connection():
+def create_database():
     try:
-        con = sqlite3.connect('test.db')
+        conn = sqlite3.connect(':memory:')
+        c = conn.cursor()
+        c.execute('''
+                CREATE TABLE BATTERY (
+                serial VARCHAR(20) PRIMARY KEY,
+                model VARCHAR(20),
+                technology VARCHAR(20),
+                manufacturer VARCHAR(20),
+                voltage_min_design INT,
+                energy_full_design INT);
+                ''')
+        c.execute('''
+                CREATE TABLE STATUS (
+                voltage_now INT,
+                condition VARCHAR(20),
+                state VARCHAR(20),
+                capacity INT,
+                power_alarm INT,
+                energy_now INT,
+                energy_full INT,
+                power_now INT,
+                serial VARCHAR(20) REFERENCES BATTERY(serial),
+                time REAL);
+                ''')
         print(sqlite3.version)
         print('OK!')
     except sqlite3.Error as e:
@@ -11,4 +34,4 @@ def create_connection():
 
 
 if __name__ == '__main__':
-    create_connection()
+    create_database()
